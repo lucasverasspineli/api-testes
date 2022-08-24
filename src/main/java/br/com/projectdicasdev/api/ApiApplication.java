b257d9c9ba0f32899932ced49d2d2529abb4b13f
Lucas Veras <lucasverasspineli@gmail.com>
2022-08-23 22:24:27 -0300

    Projeto Criado

---
+++ src/main/java/br/com/projectdicasdev/api/ApiApplication.java
@@ -0,0 +1,18 @@
+package br.com.projectdicasdev.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import br.com.projectdicasdev.api.domain.User;
+
+@SpringBootApplication
+public class ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+		
+//		User user = new User(1,"Lucas","lucas.veras@gmail.com","lucas123");
+		
+	}
+
+}
