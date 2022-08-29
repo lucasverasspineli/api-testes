7b5f6d0478d6d18f039bbca764cde55d3b310da0
Lucas Veras <lucasverasspineli@gmail.com>
2022-08-29 15:19:18 -0300

    Criando bean Model Mapper, criando classe UserDTO

--- src/main/java/br/com/projectdicasdev/api/domain/dto/UserDTO.java
+++ src/main/java/br/com/projectdicasdev/api/domain/dto/UserDTO.java
@@ -1,5 +1,7 @@
 package br.com.projectdicasdev.api.domain.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,5 +15,6 @@ public class UserDTO {
 	private Integer id;
 	private String name;
 	private String email;
+	@JsonIgnore
 	private String password;
 }
--- src/main/java/br/com/projectdicasdev/api/resources/UserResources.java
+++ src/main/java/br/com/projectdicasdev/api/resources/UserResources.java
@@ -1,5 +1,6 @@
 package br.com.projectdicasdev.api.resources;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.projectdicasdev.api.domain.User;
+import br.com.projectdicasdev.api.domain.dto.UserDTO;
 import br.com.projectdicasdev.api.services.impl.UserServiceImpl;
 
 @RestController
@@ -17,9 +19,12 @@ public class UserResources {
 	@Autowired
 	private UserServiceImpl service;
 	
+	@Autowired
+	private ModelMapper mapper;
+	
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<User> findById(@PathVariable Integer id){
-		return ResponseEntity.ok().body(service.findById(id));
+	public ResponseEntity<UserDTO> findById(@PathVariable Integer id){
+		return ResponseEntity.ok().body(mapper.map(service.findById(id),UserDTO.class));
 	}
 	
 	
