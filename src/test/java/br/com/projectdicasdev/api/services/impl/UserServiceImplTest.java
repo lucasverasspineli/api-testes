202d299fc0623c8c8fbf00c2bcfe3629629f29bb
Lucas Veras <lucasverasspineli@gmail.com>
2022-09-30 14:20:49 -0300

    Teste para metodo delete com excecao

--- src/test/java/br/com/projectdicasdev/api/services/impl/UserServiceImplTest.java
+++ src/test/java/br/com/projectdicasdev/api/services/impl/UserServiceImplTest.java
@@ -186,6 +186,17 @@ class UserServiceImplTest {
 		
 	}
 	
+	@Test
+	void deleteWithObjectNotFoundException() {
+		when(userRepository.findById(anyInt())).thenThrow(new ObjectNotFoundException(OBJETO_NAO_ENCONTRADO));
+		try {
+			 service.delete(ID);
+		} catch (Exception ex) {
+			assertEquals(ObjectNotFoundException.class, ex.getClass());
+			assertEquals(OBJETO_NAO_ENCONTRADO, ex.getMessage());
+		}
+	}
+	
 	private void startUser() {
 		user = new User(ID, NAME, EMAIL, PASSWORD);
 		userDTO = new UserDTO(ID, NAME, EMAIL, PASSWORD);
