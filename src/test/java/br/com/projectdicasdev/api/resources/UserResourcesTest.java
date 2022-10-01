a329254973b9f22d96c1a319fb3804eb347f3f68
Lucas Veras <lucasverasspineli@gmail.com>
2022-10-01 01:02:24 -0300

    Teste Classe user resource metodo findAll implementado

--- src/test/java/br/com/projectdicasdev/api/resources/UserResourcesTest.java
+++ src/test/java/br/com/projectdicasdev/api/resources/UserResourcesTest.java
@@ -3,8 +3,11 @@ package br.com.projectdicasdev.api.resources;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Assertions;
@@ -15,6 +18,7 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import br.com.projectdicasdev.api.domain.User;
@@ -58,27 +62,46 @@ class UserResourcesTest {
 	void whenFindByIdthenReturnSucess() {
 		when(service.findById(anyInt())).thenReturn(user);
 		when(mapper.map(any(), any())).thenReturn(userDTO);
-		
+
 		ResponseEntity<UserDTO> response = resource.findById(ID);
-		
+
 		assertNotNull(response);
 		assertNotNull(response.getBody());
 		assertEquals(ResponseEntity.class, response.getClass());
-		//Assegura que o Objeto que passar no corpo da requisição seja da classe UserDTO
+		// Assegura que o Objeto que passar no corpo da requisição seja da classe
+		// UserDTO
 		assertEquals(UserDTO.class, response.getBody().getClass());
-		//Verificação dos dados no corpo da requisição
+		// Verificação dos dados no corpo da requisição
 		assertEquals(ID, response.getBody().getId());
 		assertEquals(NAME, response.getBody().getName());
 		assertEquals(EMAIL, response.getBody().getEmail());
 		assertEquals(PASSWORD, response.getBody().getPassword());
-		
-	} 
 
-//	@Test
-//	void testFindAll() {
-//		fail("Not yet implemented");
-//	}
-//
+	}
+
+	@Test
+	void whenFindAllThenReturnListUserDTO() {
+		when(service.findAll()).thenReturn(List.of(user));
+		when(mapper.map(any(), any())).thenReturn(userDTO);
+
+		ResponseEntity<List<UserDTO>> listaUserDTO = resource.findAll();
+
+		assertNotNull(listaUserDTO);
+		assertNotNull(listaUserDTO.getBody());
+// 		Assegurando que o Status seja OK
+		assertEquals(HttpStatus.OK, listaUserDTO.getStatusCode());
+		assertEquals(ResponseEntity.class, listaUserDTO.getClass());
+//		Assegurando que o objeto que vem no corpo da requisição seja um ArrayList 
+		assertEquals(ArrayList.class, listaUserDTO.getBody().getClass());
+//		Verificando se o objeto que virá dentro do corpo da listaUserDTO será da mesma Classe que o UserDTO
+		assertEquals(UserDTO.class, listaUserDTO.getBody().get(INDEX).getClass());
+		assertEquals(ID, listaUserDTO.getBody().get(INDEX).getId());
+		assertEquals(NAME, listaUserDTO.getBody().get(INDEX).getName());
+		assertEquals(EMAIL, listaUserDTO.getBody().get(INDEX).getEmail());
+		assertEquals(PASSWORD, listaUserDTO.getBody().get(INDEX).getPassword());
+
+	}
+
 //	@Test
 //	void testCreate() {
 //		fail("Not yet implemented");
